caa21601518fc7287e76c208c284854b034fbe2b
Farrukh Ahmed <fah@foryouandyourcustomers.com>
2025-02-22 11:53:37 +0100

    initial commit.

---
+++ SereactTask/database/init_chroma.py
@@ -0,0 +1,11 @@
+import chromadb
+import json
+
+chroma_client = chromadb.PersistentClient(path="./chroma_db")
+collection = chroma_client.get_or_create_collection(name="product_embeddings")
+
+with open("../data/embeddings.json","r") as f:
+    embeddings_data = json.load(f)
+
+for item in embeddings_data:
+    collection.add(ids=[item["id"]], embeddings=[item["embedding"]], metadatas=[item["metadata"]])
---
+++ SereactTask/database/init_mongo.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient
+import json
+import os
+
+mongo_uri = os.getenv("MONGO_URI","mongodb://mongodb:27017/")
+client = MongoClient(mongo_uri)
+db = client["product_db"]
+collection = db["products"]
+
+with open("../data/products.json","r") as f:
+    products_data = json.load(f)
+
+collection.insert_many(products_data)
+print("MongoDB initialized with product metadata.")
